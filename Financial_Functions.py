18522f58577a78c7e7d4c9bfaa0266e977f401c5
dennis0411 <dennis810411@gmail.com>
2022-03-24 16:38:07 +0800

    ffn advanced 1

--- Financial_Functions.py
+++ Financial_Functions.py
@@ -1,20 +1,31 @@
 import ffn
-
-list = ['aapl', 'msft', 'spy']
-
-prices = ffn.get(list, start='2010-01-01')
-print(prices)
-rebase = prices.rebase()
-print(rebase)
-stats = rebase.calc_stats()
-dd_series = rebase.to_drawdown_series()
-dd = rebase.calc_max_drawdown()
+import numpy as np
 
 # reference: https://pmorissette.github.io/ffn/_modules/ffn/core.html#PerformanceStats
 
-print("try:", [stats[x].mtd for x in list])
 
+portfolio = {'spy': 0.5,
+             'agg': 0.5,
+             }
+
+portfolio_list = list(portfolio.keys())
+portfolio_weight = list(portfolio.values())
+
+prices = ffn.get(portfolio_list, start='2010-01-01')
+rebase = prices.rebase()
 
 returns_rebase = rebase.to_returns().dropna()
 
-print(returns_rebase)
+weight = np.array(portfolio_weight)
+weighted_rebase = rebase.mul(weight)
+portfolio_rebase = weighted_rebase.sum(axis=1)
+
+print(portfolio_rebase)
+
+stats = portfolio_rebase.calc_stats()
+dd_series = portfolio_rebase.to_drawdown_series()
+mdd = portfolio_rebase.calc_max_drawdown()
+
+print(stats.display())
+print(dd_series)
+print(mdd)
